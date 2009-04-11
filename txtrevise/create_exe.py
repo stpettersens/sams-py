8817a8d3470dd59ac1e709eb7bcbd85345afffd7
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2009-04-11 17:11:16 +0000

    Correction to create_exe.py. (txtrevise)

--- txtrevise/create_exe.py
+++ txtrevise/create_exe.py
@@ -1,6 +1,6 @@
 # Create Windows executable for txtrevise using
 # Py2Exe module - http://www.py2exe.org
-# Usage: python setup_exe.py py2exe
+# Usage: python create_exe.py py2exe
 from distutils.core import setup
 import py2exe
 
