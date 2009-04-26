1638bc630f52958c4d56576fcfea4e718ab655f4
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2009-04-26 12:22:06 +0000

    Added quiet mode (-q option). (txtrevise)

--- txtrevise/txtrevise.py
+++ txtrevise/txtrevise.py
@@ -15,8 +15,10 @@ version = '1.1' # Application version
 def main():
     """
     Main method
-    @return 0 exit code for application
     """
+    # Produce verbose output by default
+    global verbose; verbose = True
+    
     # Count arguments provided, if none;
     # display "No arguments.." message and usage
     if len(sys.argv) < 2:
@@ -28,20 +30,21 @@ def main():
     try:
         # Command line structure:
         # -h -f <filename> -l <line #> -m <word(s)> -r <word(s)>
-        switches, args = getopt.getopt(sys.argv[1:], "hvf:l:m:r:")
+        switches, args = getopt.getopt(sys.argv[1:], "hqf:l:m:r:")
 
         # When args are valid, perform appropriate action
         for s, a in switches:
             if s == "-h": displayUsage() # With switch "-h", display usage
             if s == "-f": filename = a # With switch "-f", specify filename
             if s == "-l": lineNo = int(a) # With switch "-l", specify line no.
-            if s == "-m": match = a # With switch, "-m", specify match word(s)
-            if s == "-r": repl = a # With switch, "-r", specify replacement
+            if s == "-m": match = a # With switch "-m", specify match word(s)
+            if s == "-r": repl = a # With switch "-r", specify replacement
+            if s == "-q": verbose = False # With switch "-q", suppress output
 
         # With necessary arguments, process file
         if len(sys.argv) > 2:
-            if sys.argv[2] == filename:
-                processFile(filename, lineNo, match, repl)
+			if filename != "":
+				processFile(filename, lineNo, match, repl)
 
     # On exception(s), display an error message and usage
     except getopt.GetoptError, ex:
@@ -92,13 +95,13 @@ def matchReplace(line, lineNo, match, repl):
     """
     # If word(s) are matched, return edited line with replacement word(s)
     if re.search(match, line):    
-        print("\nMatched at Line %d: %s" % (lineNo, line))
+        if verbose: print("\nMatched at Line %d: %s" % (lineNo, line))
         newLine = re.sub(match, repl, line)
-        print("Replaced with:\t %s" % newLine)
+        if verbose: print("Replaced with:\t %s" % newLine)
 
     # Otherwise, return same line as before
     else:
-        print("\nNo matches at Line %d." % lineNo)
+        if verbose: print("\nNo matches at Line %d." % lineNo)
         newLine = line
 
     return newLine
@@ -111,13 +114,15 @@ def displayUsage():
     print("Command line text editing tool")
     print("Copyright (c) 2009 Sam Saint-Pettersen")
     print("\nReleased under the MIT License")
-    print("\nUsage: %s [-h] -f <file> -l <line #> -m <word(s)>" % sys.argv[0])
+    print("\nUsage: %s [-h] (-q) -f <file> -l <line #> -m <word(s)>" % sys.argv[0])
     print("\t-r <word(s)>")
     print("\n\t-f: File to edit")
     print("\t-l: Line number to edit text on (starts at 1)")
     print("\t-m: Word(s) to match")
     print("\t-r: Replacement word(s) for matched word(s)")
+    print("\t-q: Quiet mode. Only output to console for errors")
     print("\t-h: This help information")
+    sys.exit()
 
 def displayError(err):
     """
