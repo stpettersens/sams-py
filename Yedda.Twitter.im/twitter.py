d5066b4267e7d5904feed350c19a8221b418e0c0
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2009-05-07 17:41:45 +0000

    Initial commit. (Yedda.Twitter.im)

---
+++ Yedda.Twitter.im/twitter.py
@@ -0,0 +1,57 @@
+"""
+Test Yedda.Twitter assembly in IronPython
+"""
+import clr
+clr.AddReference("System.Xml")
+clr.AddReference("Yedda.Twitter")
+from System.Xml import *
+from System.Diagnostics import Process
+from Yedda import Twitter
+
+p = Process()
+t = Twitter()
+
+def followers(screenName):
+	print(t.GetUserFollowersAsJSON(screenName))
+
+def real_name(IDorScreenName):
+	print(t.GetRealName(IDorScreenName))
+
+def screen_name(userID):
+	print(t.GetScreenName(userID))
+
+def user_id(screenName):
+	print(t.GetUserID(screenName))
+
+def location(IDorScreenName):
+	print(t.GetUserLoc(IDorScreenName))
+
+def biog(IDorScreenName):
+	print(t.GetUserBiog(IDorScreenName))
+
+def image(IDorScreenName):
+	url = t.GetUserImg(IDorScreenName)
+	print(url)
+	p.StartInfo.FileName = url
+	p.Start()
+
+def show(userName, password):
+	print(tw.ShowAsJSON(userName, password, userName))
+
+def each_follower(screenName):
+	followers = t.GetUserFollowersAsXML(screenName)
+	ids = followers.GetElementsByTagName("id")
+	i = 0
+	while i < ids.Count:
+		print(ids.Item(i).InnerText)
+		print(t.GetScreenName(ids.Item(i).InnerText))
+		i += 1
+
+def main():
+	print("\nRun this in IronPython interactive mode.")
+	print("ipy")
+	print(">>import twitter")
+	print(">>twitter.<command>")
+
+if __name__ == "__main__": main()
+
