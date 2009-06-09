70e2210148f28103f741d80d5746d69d48d42204
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2009-06-09 20:39:28 +0000

    Some more work. (360ify)

--- 360ify/360ify.js
+++ 360ify/360ify.js
@@ -6,29 +6,25 @@
 // @license: MIT License
 // Special thanks to Duncan Mackenzie (@duncanma) 
 // for his Xbox gamertag REST API which this script uses.
+// This work is in no way affliated with or otherwise endorsed 
+// by Microsoft Corporation.
 // http://bit.ly/xboxgtapi
 // http://duncanmackenzie.net
 // </JETPACK>
+var title = " - 360ify";
 var xboxLive = 'http://live.xbox.com';
 var xboxIcon = 'http://img8.imageshack.us/img8/7338/360ifyr.png';
-var xboxGTApi = 'http://duncanmackenzie.net/services/GetXboxInfo.aspx?Gamertag=';
+var xboxGTApi = 'http://duncanmackenzie.net/services/GetXboxInfo.aspx';
 var gamerTag = 'earlsKarma'; // Temporary, gamer tag will be able to be set
-var achievements = null;
-function openProfile() {
-	
-}
-function lastAchievement() {
-    jetpack.notifications.show(
-    {title: "Last Achievement ", body: 'Something cool!', icon: xboxIcon});
-}
-function newAchievement() {
-	
+function showProfile() {
+    // Cannot make cross domain requests with current Jetpack like you can with Greasemonkey
+    // work around is to try to use Greasemonkey for getting the feeds (?)
 }
 jetpack.statusBar.append({
     html: '<img src="' + xboxIcon + '">',
     width: 16,
     onReady: function(doc) {
     $(doc).find("img").click(function() {
-        lastAchievement();
+        showProfile();
     });
 }});
