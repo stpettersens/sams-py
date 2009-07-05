170cbdd0efb952756d5e6332801bfade6a2c6a23
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2009-07-05 05:48:37 +0000

    First release of pollen command. (UbiquityCmd)

--- UbiquityCmd/pollenforecast.js
+++ UbiquityCmd/pollenforecast.js
@@ -12,24 +12,31 @@ CmdUtils.CreateCommand({
   preview: function(pblock, args) {
   	var data = {};
 	data.query = args.object.text;
-  	var url = "http://www.gm.tv/index.cfm?article=2932&symbolPrefix=p" 
-	+ "&postcode=" + data.query + "&articleID=2932&showWorldRegion=3&showTab=1";
-	$.get(url, function(page) {
-		var formatted = $(page).find(".weatherText").text()
-		.replace(/Weather UV Index Pollen Count-/, "");
-		formatted = formatted.replace(/United Kingdom - Europe/, "")
-		formatted = formatted.replace(/\d+\s{1}\w{3}/g, ""); // Strip out dates, just keep days
-		formatted = formatted.replace(/HIGH/g, 
-		"<span style='font-weight: bold; color: #ff0000;'>H</span>");
-		formatted = formatted.replace(/MEDIUM/g,
-		"<span style='font-weight: bold; color: #ff6600;'>M</span>");
-		formatted = formatted.replace(/Pollen forecast is not available./,
-		"<span style='font-style: italic; color: #999999'>No forecast</span>");
-		data.forecast = formatted;
-    	pblock.innerHTML = CmdUtils.renderTemplate(
-		"The pollen count forecast for UK <b>${query}</b> from GMTV" +
-		"<p style='font-size: 16px'>${forecast}</p>", data);
-	});
+  	var baseUrl = "http://www.gm.tv/index.cfm?article=2932&symbolPrefix=p";
+	if (data.query.search(/[a-z]{1,2}\d{1,2}\s?\d{0,1}[a-z]{0,2}/ig) != -1) {
+		var url = baseUrl + "&postcode=" + data.query + "&articleID=2932&showWorldRegion=3&showTab=1";
+		$.get(url, function(page){
+			var formatted = $(page).find(".weatherText").text()
+			.replace(/Weather UV Index Pollen Count-/, ""); // Strip out redundant text
+			formatted = formatted.replace(/United Kingdom - Europe/, "") 
+			formatted = formatted.replace(/\d+\s{1}\w{3}/g, ""); // Strip out dates, just keep days
+			formatted = formatted.replace(/not be/, "could not be"); // Correct grammar for errors
+			formatted = formatted.replace(/Select[\w\s\n\-\.]*/gm, "") // Strip out any verbose errors
+			formatted = formatted.replace(/HIGH/g,
+			 "<span style='font-size: 20px; font-weight: bold; color: #ff0000;'>H</span>");
+			formatted = formatted.replace(/MEDIUM/g, 
+			"<span style='font-size: 20px; font-weight: bold; color: #ff6600;'>M</span>");
+			formatted = formatted.replace(/Pollen forecast is not available./, 
+			"<span style='font-style: italic; color: #999999'>No forecast</span>");
+			data.forecast = formatted;
+			pblock.innerHTML = CmdUtils.renderTemplate(
+			"The pollen count forecast for <span style='font-weight: bold'>" +
+			"${query}</span> from GMTV" +
+			"<p style='font-size: 16px'>${forecast}</p>", data);
+		});
+	}
+	else pblock.innerHTML = CmdUtils.renderTemplate("Hit <span style='font-weight: bold'>" 
+	+ "enter</span> for the pollen count forecast for UK from GMTV");
   },
   execute: function(object, args){
   	var url = "http://www.gm.tv/index.cfm?article=2932&symbolPrefix=p" 
