911052f358511c62b537ced73773d5441786ab04
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2010-01-24 23:34:56 +0000

    Got Ctrl-C to terminate server working. :)

--- smtp.server/smtps.py
+++ smtp.server/smtps.py
@@ -11,7 +11,7 @@ import signal
 import threading
 import datetime
 import re
-        
+
 class SMTPCommand:
     def helo(self):
         # ...
@@ -23,57 +23,23 @@ class SMTPCommand:
     def unrecognised(self):
         pass
         
-class SMTPServer(threading.Thread):
-    def __init__(self):
-        self.Name = 'SMTP server'
-        threading.Thread.__init__(self)
-        
-    def run(self):
-        print(__doc__)
-        self.listen()
-        
-    def listen(self, port=26): # Change to port 25 later
-        received = returned = ''
-        print('Listening on port {0}...\n'.format(port))
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', port))
-        s.listen(1)
-        conn, addr = s.accept()
-        while 1:
-            if self.state == 0:
-                date = datetime.datetime.now()
-                # Relay connected information to client
-                conn.send('220 {0} {1}\r\n'.format(self.Greeting, date))
-                print('>> {0} connected.\n'.format(addr))
-                self.state = 1 # Shift to ready state (1)
-            chunk = conn.recv(1024)
-            received += chunk
-            # Wait for command termination characters (CR+LF) before continuing
-            if received.endswith('\r\n'):
-                print('>> {0}'.format(received))
-                returned = self.parseCommand(received)
-                print('<< {0}'.format(returned))
-                conn.send(returned)
-                received = ''
-            if not chunk or returned == self.ExitMsg: break
-        conn.close()
-
-class Spaghetti:
+class SMTPServerSW(threading.Thread):
     def __init__(self):
         self.Name = 'SMTP server'
         self.Vers = '0.1'
         self.Greeting = '{0} v{1}'.format(self.Name, self.Vers)
         self.ExitMsg = 'Goodbye.\r\n'
         self.state = 0 # Start in listen state (0)
-        print(__doc__)
+        threading.Thread.__init__(self)
+        
+    def run(self):
         self.listen()
         
-    def parseCommand(self, received):
-        command = '' #!
-        return 'blah' #!
+    def parseCommand(self, command):
+        return SMTPCommand().helo()
         
     def listen(self, port=26): # Change to port 25 later
-        received = returned = ''
+        command = returned = ''
         print('Listening on port {0}...\n'.format(port))
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('', port))
@@ -87,15 +53,31 @@ class Spaghetti:
                 print('>> {0} connected.\n'.format(addr))
                 self.state = 1 # Shift to ready state (1)
             chunk = conn.recv(1024)
-            received += chunk
+            command += chunk
             # Wait for command termination characters (CR+LF) before continuing
-            if received.endswith('\r\n'):
-                print('>> {0}'.format(received))
-                returned = self.parseCommand(received)
+            if command.endswith('\r\n'):
+                print('>> {0}'.format(command))
+                returned = self.parseCommand(command)
                 print('<< {0}'.format(returned))
                 conn.send(returned)
-                received = ''
+                command = ''
             if not chunk or returned == self.ExitMsg: break
         conn.close()
-       
-if __name__ == '__main__': SMTPServer().start()
+        
+class SMTPServer:
+    def __init__(self):
+        self.control = 0 # Initiate server application run state (0)
+        print(__doc__)
+        print('Hold Ctrl-C to terminate.')
+        SMTPServerSW().setDaemon(True) 
+        SMTPServerSW().start()
+        while self.control == 0:
+            signal.signal(signal.SIGINT, self.quit)
+            if self.control == 1: break
+        sys.exit(0)
+        
+    def quit(self, signum, frame):
+        print('Server terminated.')
+        self.control = 1 # Change to application quit-ready state (1)
+    
+if __name__ == '__main__': SMTPServer()
