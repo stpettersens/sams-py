78306f3da54aca224c44d38e604d2dc0fbb46301
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2010-05-31 23:03:06 +0000

    SMTPS project needs to be tidied up to be neater like Automaton. AIS_Engine executes commands in AIS_Command, a class with a dictionary of defined and builtins.

--- automaton/automaton.py
+++ automaton/automaton.py
@@ -16,17 +16,34 @@ import datetime
 import re
 import Queue
 
-class ClientThread(threading.Thread):
+class AIS_Command:
+	pass
+	
+class AIS_Engine:
+	pass
+	
+class ScriptThread(threading.Thread):
 	"""
-	Client thread to connect to host server
+	Script thread to execute a script on host machine
+	(May be more than one instance)
 	"""
-	def __init__(self, host, port, script):
+	pass
+
+class ConnectionThread(threading.Thread):
+	"""
+	Connection thread to connect to target host
+	(One and only instance)
+	"""
+	def __init__(self, debug, host, port, script, max_conc):
 		"""
-		Initialization method for client thread
+		Initialization method for connection thread
 		"""
+		self.debug = debug
 		self.host = host
 		self.port = port
 		self.script = script
+		self.max_conc = max_conc
+		self.scriptPool = Queue.Queue(0)
 		threading.Thread.__init__(self)
 		
 	def run(self):
@@ -34,20 +51,23 @@ class ClientThread(threading.Thread):
 		
 	def connect(self):
 		"""
-		Connect to host server
+		Connect to target host
 		"""
 		print('\nConnecting to {0}:{1}...'.format(self.host, self.port))
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((self.host, self.port))
-		print('\nConnected.')
-		s.send('Hello from Automaton!')
-		data = s.recv(1024)
-		s.close()
-		print('Received {0}'.format(data))
+		# ... Establish connection session dohickey
+		for i, s in enumerate(self.script):
+			#... script execution thread
+			ScriptThread(s).start()
+			if i > max_conc: 
+				# ... queue remaining scripts (that is, its index (i))
+	
 		
 class Automaton:
 	"""
 	Automation instance class
+	(One and only instance)
 	"""
 	def __init__(self):
 		"""
@@ -57,12 +77,12 @@ class Automaton:
 		self.Name = 'Automaton'
 		self.Vers = '1.0'
 		# 
-		self.MAX_SCRIPTS = 5 # Maximum allowable concurrent scripts (5)
-		self.host = 'localhost' # Host to default to ('localhost')
+		self.max_conc = 5 # Maximum allowable concurrent scripts (5)
+		self.host = 'localhost' # Host to default to ('localhost'/127.0.0.1)
 		self.port = 8282 # Port number to default to (8282, no sudo)
 		self.script = [] # Default script is blank
 		self.termSig = False # Signal to terminate application, start false
-		self.enableDebug = False # Enable debugging, default as false
+		self.debug = False # Enable debugging, default as false
 		
 		# Handle command line options
 		try:
@@ -74,12 +94,12 @@ class Automaton:
 				elif o == '-p':
 				    self.port = int(a)
 				elif o == '-s':
-					for x in a.split('+'): self.script.append(x)
+					for s in a.split('+'): self.script.append(s)
 				# Switches without arguments
 				elif o == '-i':
 					self.displayCmdLineOps()
                 #elif o == '-d':
-                #	self.enableDebug = True
+                	#self.debug = True
 		
 		# For invalid command line options, show general error message			
 		except getopt.GetoptError, err:
@@ -88,12 +108,13 @@ class Automaton:
 			
 		# Specifically for an invalid port number, show more specific error message
 		except ValueError:
-			print('\nError: Port must be an unsigned integer value, not \'{0}\'.'.format(a))
+			print('\nError: Port number must be unsigned integer value, not \'{0}\'.'.format(a))
 			self.displayCmdLineOps()
 		
 		print(__doc__)
 		print('Hold Ctrl-C to terminate.')
-		ClientThread(self.host, self.port, self.script).start()
+		ConnectionThread(self.debug, self.host, self.port, 
+		self.script, self.max_conc).start()
 		while not self.termSig:
 			signal.signal(signal.SIGINT, self.quit)
 			if self.termSig: sys.exit(0)
@@ -104,7 +125,7 @@ class Automaton:
 		"""
 		print(__doc__)
 		print('Usage: {0} [-i][-v][-b][-c]'.format(sys.argv[0]) 
-		+ '\n[-d -h <hostname> -p <port number> -s <script+(script+)> -t <# threads >]\n')
+		+ '\n[-d -h <hostname> -p <port number> -s <script+(script+)> (-q)]\n')
 		print('-i: Display this information and exit.')
 		print('-v: Display version information and exit.')	
 		print('-b: Display built-in commands.')
@@ -112,8 +133,9 @@ class Automaton:
 		print('-d: Display debug information while running.')
 		print('-h: Hostname to connect to. (\'localhost\' if omitted)')
 		print('-p: Listen on specified port number. (8282 if omitted)')
-		print('-s: Script(s) to run; *.ais file(s). (Mandatory arg)')
-		print('-t: Number of script threads to run concurrently on hosts. (<= 5)\n')
+		print('-s: Script(s) to execute; *.ais file(s). (Mandatory arg)')
+		print('-q: Queue each script, rather than execute 5 at a time.\n')
+		sys.exit(1)
 
 	def	displayInfo(self):
 		pass
