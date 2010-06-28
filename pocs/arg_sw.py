395b08e1f64baed65a1d70dd875120fab9a6499d
s.stpettersen <s.stpettersen@a6fdd994-22a9-11de-b851-79cad7b500d3>
2010-06-28 22:51:47 +0000

    Some good progress. Going with JSON over cPickle.

---
+++ pocs/arg_sw.py
@@ -0,0 +1,21 @@
+# Use a Python dictionary
+# with command line options and arguments
+
+import sys, getopt
+
+def doSomething():
+    print('I just did something')
+    
+values = ({
+		'-h': 'localhost', #host 
+		'-p': 8282, #port
+		'-d': '!doSomething()' #doSomething()
+		})
+		
+opts, args = getopt.getopt(sys.argv[1:],'h:p:d')
+for o, a in opts:
+    if a != '': values[o] = a
+    x = values.get(o)
+    
+if x.startswith('!'): eval(x.replace('!', ''))
+else: print(x)
---
+++ pocs/jsonjar.py
@@ -0,0 +1,28 @@
+"""
+Demonstrate JSON module
+with an application to serialize and deserialize an array (list) of integers.
+Similar to Pickle, but I can have neater structures
+"""
+import json
+
+def showError():
+    print('You must enter either 1 or 2!')
+
+def doSerialize():
+    myIntegers = []
+    for i in xrange(5):
+        myIntegers.append(int(raw_input('Enter an integer: ')))
+        
+    print(myIntegers)
+    writeFile = open('jsonInts.json', 'w')
+    json.dump(myIntegers, writeFile)
+
+def doDeserialize():
+    readFile = open('jsonInts.json', 'r')
+    myIntegers = json.load(readFile)
+    print(myIntegers)
+
+input = str(raw_input('Serialize new (1) or Deserialize existing (2) ?: '))
+options = ({'1': '!doSerialize()', '2': '!doDeserialize()'})
+sel = options.get(input, '!showError()')
+eval(sel.replace('!',''))
